32f59748371a354266fe1d6eb415fe9388890417
danrleydimas <danrley-dimas@hotmail.com>
2024-12-12 20:02:07 -0300

    instagram form

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,114 +1,70 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+const src = '/imgs/insta-img.png'
+const srcImgForm ='/imgs/insta-logo.png'
+const srcImgApple ='/imgs/apple.png'
+const srcImgGoole = '/imgs/google.png'
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="h-screen w-screen grid grid-cols-1 md:grid-cols-2 grid-rows-6">
+        <div className="row-span-5 justify-end hidden md:flex">
+           <Image src={src} alt="instagram-celphone" className="w-max-96 " width="400" height="400"/>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="row-span-5 flex-col grid-cols-1 justify-center items-center">
+          <form className="w-80 h-4/6 flex-col justify-center items-center border rounded-sm px-10 py-4 gap-5 mt-24 ml-auto mr-auto md:ml-4">
+             <Image src={srcImgForm} alt="logo" className="w-52 w-max-52" width="400" height="400"/>
+             <input className="bg-slate-50 h-9 w-full pl-3 border rounded-sm gap-3" placeholder="Número de Telefone, e-mail" />
+             <input  className="bg-slate-50 h-9 w-full pl-3 border rounded-sm mt-2" placeholder="Senha"/>
+             <button className="bg-blue-400 w-full rounded-md h-8 text-slate-50 mt-2 hover:bg-blue-300" >entrar</button>
+             <div className="flex-col items-center">
+              <div className="flex justify-center">
+                <a className="text-blue-800 font-bold coursor-pointer"> Logar com facebook</a>
+              </div>
+              <div className="flex justify-center">
+                <a className="text-blue-800 text-sm coursor-pointer">Esqueci minha senha</a>
+              </div>
+             </div>
+          </form>
+          <div className="border w-80 my-4 text-center py-4  md:ml-4 ml-auto mr-auto">
+             <p>Não tem conta? <a className="text-blue-400" >Cadastrar-se</a></p>
+          </div>
+          <div className="flex w-80 justify-center md:ml-4 ml-auto mr-auto ">
+            <p>Baixe o Aplicativo</p>
+          </div>
+          
+          <div className="flex  w-80 py-4 justify-center md:ml-4 ml-auto mr-auto">
+            
+             <Image src={srcImgApple} alt="apple" className="h-max-10" width="100" height="40"/>
+             <Image src={srcImgGoole} alt="google" className="h-max-10" width="100" height="40"/>
+          </div>
+        </div>
+      
+          <div className="row-span-1 col-span-1 mt-12">
+            <ul className="flex flex-wrap gap-3 justify-center">
+              <li className="text-slate-800 text-xs">Meta</li>
+              <li className="text-slate-800 text-xs">Sobre</li>
+              <li className="text-slate-800 text-xs">Blog</li>
+              <li className="text-slate-800 text-xs">Carreiras</li>
+              <li className="text-slate-800 text-xs">Ajuda</li>
+              <li className="text-slate-800 text-xs">API</li>
+              <li className="text-slate-800 text-xs">Privacidade</li>
+              <li className="text-slate-800 text-xs">Termos</li>
+              <li className="text-slate-800 text-xs">Localizações</li>
+              <li className="text-slate-800 text-xs">Instagram  Lite</li> 
+              <li className="text-slate-800 text-xs">Threads</li>
+              <li className="text-slate-800 text-xs">Carregamento de contatos e não usuários</li>
+              <li className="text-slate-800 text-xs">Meta Verified</li>
+
+            </ul>
+            <ul className="flex flex-wrap gap-5 justify-center mt-3">
+              <li className="text-slate-800 text-xs">Português (Brasil)</li>
+              <li className="text-slate-800 text-xs">© 2024 Instagram from Meta</li>
+            </ul>
+          </div>
+       
+      </div>
+
+ 
+    
   );
 }
